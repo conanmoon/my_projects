eafe8e6f28e87385fa30c4ffad64c6841d6a7163
conanmoon <72179917+conanmoon@users.noreply.github.com>
2020-11-09 21:03:13 +0900

    1

--- calculator_package/setup.py
+++ calculator_package/setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
  
 setuptools.setup(
-    name="calculator_package_seungtaemoon", # pip 으로 설치한 패키지 이름입니다
+    name="calculator_package_mst", # pip 으로 설치한 패키지 이름입니다
     version="0.0.4",
     author="seungtaemoon",
     author_email="conanmoon@gmail.com",
