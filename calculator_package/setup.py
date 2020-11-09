a25f8615b72a81c295be0c87ba0158ff11928d47
conanmoon <72179917+conanmoon@users.noreply.github.com>
2020-11-09 17:22:02 +0900

    calculator package update

--- calculator_package/setup.py
+++ calculator_package/setup.py
@@ -5,13 +5,13 @@ with open("README.md", "r") as fh:
  
 setuptools.setup(
     name="calculator-pkg-seungtaemoon", # pip 으로 설치한 패키지 이름입니다
-    version="0.0.1",
-    author="Example Author",
+    version="0.0.2",
+    author="seungtaemoon",
     author_email="conanmoon@gmail.com",
     description="A calculator package",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/seungtaemoon/my_projects",
+    url="https://github.com/seungtaemoon/my_projects/calculator_package",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
