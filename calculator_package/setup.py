3acef5f357ca9ac075acabeb1900189b05f207a0
conanmoon <72179917+conanmoon@users.noreply.github.com>
2020-11-09 18:07:28 +0900

    calculator package update

--- calculator_package/setup.py
+++ calculator_package/setup.py
@@ -11,7 +11,7 @@ setuptools.setup(
     description="A calculator package",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/seungtaemoon/my_projects/calculator_package/calculator",
+    url="https://github.com/seungtaemoon/my_projects/calculator_package/",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
