af222842e15d9d424b57cdaacbc0711caa2c3f55
conanmoon <72179917+conanmoon@users.noreply.github.com>
2020-11-09 18:58:33 +0900

    calculator update

--- calculator_package/setup.py
+++ calculator_package/setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
  
 setuptools.setup(
-    name="calculatorpkgseungtaemoon", # pip 으로 설치한 패키지 이름입니다
+    name="calculator_package_seungtaemoon", # pip 으로 설치한 패키지 이름입니다
     version="0.0.4",
     author="seungtaemoon",
     author_email="conanmoon@gmail.com",
@@ -18,5 +18,5 @@ setuptools.setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    python_requires='>=3.6',
+    python_requires='>=3.6'
 )
